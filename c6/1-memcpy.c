b229154ca23b9202c900abcb4c1cab8d5d7bc917
Bayadir Aldossari <13222@holbertonstudents.com>
2026-09-01 14:26:08 +0000

    add task that i make

---
+++ c6/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: memory area where bytes are copied to
+ * @src: memory area from where bytes are copied
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
